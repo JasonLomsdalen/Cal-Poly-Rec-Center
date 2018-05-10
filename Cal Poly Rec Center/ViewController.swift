a8583bf1be4929cc4b7ac2587261cdb8a2393fea
JasonLomsdalen <JasonComputer@jasons-macbook-pro.local>
2018-05-10 10:44:55 -0700

    Initial Commit

---
+++ Cal Poly Rec Center/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cal Poly Rec Center
+//
+//  Created by Jason Lomsdalen on 5/10/18.
+//  Copyright © 2018 Jason / Kirsten Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
