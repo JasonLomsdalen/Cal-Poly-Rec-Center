0e9a4ec94f4f15c07894e26214f8107d41511932
Kirsten Loechl <kirsten.loechl7@gmail.com>
2018-05-30 21:53:22 -0700

    added firebase integration (pod file ect)

---
+++ Cal Poly Rec Center/ChartViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ChartViewController.swift
+//  Cal Poly Rec Center
+//
+//  Created by Kirsten M Loechl on 5/29/18.
+//  Copyright © 2018 Jason / Kirsten Productions. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class ChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Cal Poly Rec Center/ParseRecCenterJSON/ParseRecCenterJSON/ViewController.swift
+++ Cal Poly Rec Center/ParseRecCenterJSON/ParseRecCenterJSON/ViewController.swift
@@ -61,21 +61,18 @@ class ViewController: UIViewController {
         task.resume()
     }
     func uploadDailyAveragesToFirebase() {
-        print ("test1")
+       
         let session = URLSession(configuration: URLSessionConfiguration.default)
-        print ("test2")
+       
         let request = URLRequest(url: URL(string: dailyAvg)!)
-        print ("test3")
+        
         let task: URLSessionDataTask = session.dataTask(with: request)
         { (receivedData, response, error) -> Void in
-            print ("test4")
             if let data = receivedData {
                 do {
                     let decoder = JSONDecoder()
                     let hourlyAvgService2 = try decoder.decode([HourlyAvg].self, from: data)
-                    print ("test5")
                    // self.ref = Database.database().reference().child("dailyhourlyAvg")
-                    print ("test6")
                     for item in hourlyAvgService2 {
                         // item.version = item.version?.replacingOccurrences(of: ".", with: "")
                         let schoolRef = self.ref?.child(item.version!)
